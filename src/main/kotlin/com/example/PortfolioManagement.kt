8e0e63db04b51ffc51288ccfb8b8e554cabe569c
ashishku <ashishku@thoughtworks.com>
2022-02-15 13:54:12 +0530

    Adds project skeleton for portfolio management service

---
+++ src/main/kotlin/com/example/PortfolioManagement.kt
@@ -0,0 +1,11 @@
+package com.example
+
+class PortfolioManagement {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Portfolio management")
+            args.forEach(::println)
+        }
+    }
+}
