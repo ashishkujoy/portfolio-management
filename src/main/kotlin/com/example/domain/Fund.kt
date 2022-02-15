7cc3fef8f84cd1a8612c08acf69f4b46d5044140
ashishku <ashishku@thoughtworks.com>
2022-02-15 20:45:28 +0530

    Implements equal and hashcode for Fund

--- src/main/kotlin/com/example/domain/Fund.kt
+++ src/main/kotlin/com/example/domain/Fund.kt
@@ -3,7 +3,7 @@ package com.example.domain
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-class Fund(private val name: String, stocks: Set<Stock>) {
+class Fund(val name: String, stocks: Set<Stock>) {
     private val currentStocks = stocks.toMutableSet()
 
     fun addStock(stock: Stock): Fund {
@@ -16,5 +16,19 @@ class Fund(private val name: String, stocks: Set<Stock>) {
         val commonFunds = currentStocks.intersect(otherFund.currentStocks).size
 
         return BigDecimal(((2.00 * commonFunds) / totalFunds) * 100.00).setScale(2, RoundingMode.CEILING)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Fund -> other.name == name && other.currentStocks == currentStocks
+            else -> false
+        }
+
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + currentStocks.hashCode()
+        return result
     }
 }
--- src/test/kotlin/com/example/domain/FundTest.kt
+++ src/test/kotlin/com/example/domain/FundTest.kt
@@ -1,11 +1,44 @@
 package com.example.domain
 
-import io.kotest.matchers.equality.shouldBeEqualToComparingFields
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
 
 class FundTest {
+
+    @Test
+    fun `funds having same name and current stocks should be same`() {
+        val lngStock = Stock("PETRONET LNG LIMITED")
+        val adaniStock = Stock("ADANI ENTERPRISES LIMITED")
+
+        val iciciFund = Fund("ICICI_PRU_NIFTY_NEXT_50_INDEX", setOf(lngStock, adaniStock))
+        val anotherIciciFund = Fund("ICICI_PRU_NIFTY_NEXT_50_INDEX", setOf(lngStock, adaniStock))
+
+        iciciFund shouldBe anotherIciciFund
+    }
+
+    @Test
+    fun `funds having different name but same current stocks should be different`() {
+        val lngStock = Stock("PETRONET LNG LIMITED")
+        val adaniStock = Stock("ADANI ENTERPRISES LIMITED")
+
+        val iciciFund = Fund("ICICI_PRU_NIFTY_NEXT_50_INDEX", setOf(lngStock, adaniStock))
+        val miraefund = Fund("MIRAE_ASSET_EMERGING_BLUECHIP", setOf(lngStock, adaniStock))
+
+        iciciFund shouldNotBe miraefund
+    }
+
+    @Test
+    fun `fund having same name but different current stocks should be different`() {
+        val lngStock = Stock("PETRONET LNG LIMITED")
+        val adaniStock = Stock("ADANI ENTERPRISES LIMITED")
+
+        val iciciFund = Fund("ICICI_PRU_NIFTY_NEXT_50_INDEX", setOf(lngStock, adaniStock))
+        val anotherIciciFund = Fund("ICICI_PRU_NIFTY_NEXT_50_INDEX", setOf(lngStock))
+
+        iciciFund shouldNotBe anotherIciciFund
+    }
 
     @Test
     fun `add given stock`() {
@@ -15,12 +48,9 @@ class FundTest {
 
         fund.addStock(adaniStock)
 
-        fund.shouldBeEqualToComparingFields(
-            Fund(
-                name = "ICICI_PRU_NIFTY_NEXT_50_INDEX",
-                stocks = setOf(lngStock, adaniStock)
-            ),
-            ignorePrivateFields = false
+        fund shouldBe Fund(
+            name = "ICICI_PRU_NIFTY_NEXT_50_INDEX",
+            stocks = setOf(lngStock, adaniStock)
         )
     }
 
